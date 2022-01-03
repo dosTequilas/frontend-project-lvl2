6a9fb3c26806f0f8425869e685963fe45f3f6756
Evgeniy Larin <nothinghill15@gmail.com>
2022-01-03 20:57:20 +0300

    3rd stage complete

--- src/genDiff.js
+++ src/genDiff.js
@@ -10,21 +10,7 @@ const diff = (file1, file2) => {
   const obj2 = JSON.parse(readFileSync(path2));
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
-  const commonKeys = _.union(keys1, keys2).sort();
-  // const result = [];
-
-  // for (let key of commonKeys) {
-  //   if (obj1[key] === obj2[key]) {
-  //     result.push(`${key}: ${obj1[key]}`);
-  //   } else if (!obj2[key]) {
-  //     result.push(`- ${key}: ${obj1[key]}`);
-  //   } else if (!obj1[key]) {
-  //     result.push(`+ ${key}: ${obj2[key]}`);
-  //   } else if (obj1.hasOwnProperty(key) && obj2.hasOwnProperty(key) && obj1[key] !== obj2[key]) {
-  //     result.push(`- ${key}: ${obj1[key]}`);
-  //     result.push(`+ ${key}: ${obj2[key]}`);
-  //   }
-  // }
+  const commonKeys = _.sortBy(_.union(keys1, keys2));
 
   let result = commonKeys.map((key) => {
     if (obj1[key] === obj2[key]) {
