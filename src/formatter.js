86b8027679f490dd290c96c988afa44f5f3f6104
Evgeniy Larin <nothinghill15@gmail.com>
2022-02-20 11:47:41 +0300

    works

--- src/formatter.js
+++ src/formatter.js
@@ -1,30 +1,59 @@
+// import { keys } from 'lodash';
+
+import _ from 'lodash';
+
+const getTabSize = (currentDepth, multiplier = 2) => {
+  const space = ' ';
+  const result = space.repeat(currentDepth * multiplier);
+  return result;
+};
+
+const getFormattedValue = (value, depth) => {
+  const tab = getTabSize(depth, 4);
+  const tabClose = getTabSize(depth - 1, 4);
+
+  if (!_.isObject(value)) {
+    return String(value);
+  }
+  const entries = Object.entries(value);
+
+  const result = entries.map(([key, innerValue]) => {
+    const tempString = `${tab}  ${key}: ${getFormattedValue(innerValue, depth + 1)}`;
+    return tempString;
+  });
+  return ['{', ...result, `${tabClose}  }`].join('\n');
+};
+
 const formatter = (tree) => {
   const iter = (node, depth) => {
     const arrMap = node.flatMap((key) => {
       if (key.type === 'nested') {
         const formattedChildren = iter(key.children, depth + 1);
-        const tab = ''; //функцию для вычисления отступа
+        const tab = getTabSize(depth);
         const result = `${tab}  ${key.name}: {\n${formattedChildren}\n${tab}}`;
-        return result; // не массив, а строку, добавить отступы
+        return result;
       }
       if (key.type === 'unchanged') {
-        const tab = '';
-        const result = `${tab}  ${key.name}: ${key.value}`;
+        const tab = getTabSize(depth);
+        const result = `${tab}  ${key.name}: ${getFormattedValue(key.value, depth)}`;
         return result;
       }
       if (key.type === 'deleted') {
-        const tab = '';
-        const result = `${tab}- ${key.name}: ${key.value}`;
+        const tab = getTabSize(depth);
+        const result = `${tab}- ${key.name}: ${getFormattedValue(key.value, depth)}`;
         return result;
       }
       if (key.type === 'added') {
-        const tab = '';
-        const result = `${tab}+ ${key.name}: ${key.value}`;
+        const tab = getTabSize(depth);
+        const result = `${tab}+ ${key.name}: ${getFormattedValue(key.value, depth)}`;
         return result;
       }
       if (key.type === 'changed') {
-        const tab = '';
-        const result = `${tab}- ${key.name}: ${key.oldValue}\n${tab}+ ${key.name}: ${key.newValue}`;
+        const tab = getTabSize(depth);
+        const result = `${tab}- ${key.name}: ${getFormattedValue(key.oldValue, depth)}\n${tab}+ ${
+          key.name
+        }: ${getFormattedValue(key.newValue, depth)}`;
+        return result;
       }
       return key.name;
     });
